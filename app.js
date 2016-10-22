1c4e1b0ff6b558b9fb273ae2fdb07d603d254ac0
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2016-10-22 12:02:57 +0200

    i dalje problem sa back

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var path = require('path');
 var express = require('express');
 var app = express();
-//app.use(express.static('unbundled'));
+app.use(express.static('unbundled'));
 app.use(express.static(path.join(__dirname, 'unbundled')));
 
 
